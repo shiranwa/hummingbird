5abfda86d5145dadd1b78ef5217ddcc756a95eac
Thomas McNiven <hello@vevix.net>
2015-01-23 23:42:00 +1000

    implement feed posting on groups

--- app/frontend/app/controllers/story.js
+++ app/frontend/app/controllers/story.js
@@ -50,6 +50,14 @@ export default Ember.ObjectController.extend(HasCurrentUser, {
 
       var self = this;
       this.store.find('user', this.get('currentUser.id')).then(function(user) {
+        // group permission check
+        var group = self.get('model.group');
+        if (group && !group.isMember(user)) {
+          self.set('reply', '');
+          Messenger().error("You must be a member of the group.");
+          return;
+        }
+
         var reply = self.store.createRecord('substory', {
           story: self.get('model'),
           user: user,
